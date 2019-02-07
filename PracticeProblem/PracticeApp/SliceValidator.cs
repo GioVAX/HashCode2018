ea525ec0e5a96b0d330cab0ca02f35a244f22230
Giovanni Pezzino <giovannipezzino.64@gmail.com>
2019-02-07 14:19:24 +0000

    Moved tolerance for validator into configuration of constructor

--- PracticeProblem/PracticeApp/SliceValidator.cs
+++ PracticeProblem/PracticeApp/SliceValidator.cs
@@ -5,36 +5,39 @@ namespace PracticeApp
     public class SliceValidator
     {
         private readonly int[,] _sumValues;
+        private readonly int _width;
+        private readonly int _height;
+        private readonly int _minSize;
 
-        public SliceValidator(int[,] values)
+        public SliceValidator(int[,] values, int minSize)
         {
-            var w = values.GetLength(0);
-            var h = values.GetLength(1);
-            _sumValues = new int[w, h];
+            _width = values.GetLength(0);
+            _height = values.GetLength(1);
+            _sumValues = new int[_width, _height];
 
             var rowSum = 0;
-            for (var i = 0; i < w; ++i)
+            for (var i = 0; i < _width; ++i)
                 _sumValues[i, 0] = (rowSum += values[i, 0]);
 
-            for (var i = 1; i < h; ++i)
+            for (var i = 1; i < _height; ++i)
             {
                 rowSum = 0;
-                for (var j = 0; j < w; ++j)
+                for (var j = 0; j < _width; ++j)
                     _sumValues[j, i] = (rowSum += values[j, i]) + _sumValues[j, i - 1];
             }
+
+            _minSize = minSize;
         }
 
-        public bool IsSliceValid(Slice slice, int maxValue)
+        public bool IsSliceValid(Slice slice)
         {
             var leftX = slice.Origin.X;
             var topY = slice.Origin.Y;
             var rightX = leftX + slice.Width - 1;
             var bottomY = topY + slice.Height - 1;
 
-            if (leftX < 0 || topY < 0)
+            if (leftX < 0 || topY < 0 || rightX >= _width || bottomY >= _height)
                 return false;
-
-
 
             var baseValue = _sumValues[rightX, bottomY];
             if (leftX != 0)
@@ -46,7 +49,7 @@ namespace PracticeApp
             if (leftX != 0 && topY != 0)
                 baseValue += _sumValues[leftX - 1, topY - 1];
 
-            return Math.Abs(baseValue) <= maxValue;
+            return Math.Abs(baseValue) <= slice.Height * slice.Width - _minSize;
         }
     }
 }
--- PracticeProblem/PracticeAppUnitTests/SliceValidatorUnitTests.cs
+++ PracticeProblem/PracticeAppUnitTests/SliceValidatorUnitTests.cs
@@ -13,39 +13,40 @@ namespace PracticeAppUnitTests
         {
             var ingredients = new[,]
             {
-                {1, 1, 1, 1, 1},
+                {1,  1,  1,  1, 1},
                 {1, -1, -1, -1, 1},
-                {1, 1, 1, 1, 1}
+                {1,  1,  1,  1, 1}
             };
 
-            _sut = new SliceValidator(ingredients);
+            _sut = new SliceValidator(ingredients, 2);
         }
-        private bool CheckSlice(int topX, int topY, int width, int height, int tolerance)
+        private bool CheckSlice(int topX, int topY, int width, int height)
         {
             var slice = new Slice(new Point(topX, topY), new SliceTemplate(width, height));
-            return _sut.IsSliceValid(slice, tolerance);
+            return _sut.IsSliceValid(slice);
         }
 
         [Theory]
         [InlineData(-1, 0, 2, 1)]
         [InlineData(0, -1, 2, 1)]
+        [InlineData(1, 1, 1, 8)]
+        [InlineData(1, 1, 12, 2)]
+        [InlineData(-10, -10, 120, 200)]
         public void ValidatingSliceOutsideOfPizza_ShouldReturnFalse(int topX, int topY, int width, int height) =>
-            CheckSlice(topX, topY, width, height, 0).Should().BeFalse();
-
-
+            CheckSlice(topX, topY, width, height).Should().BeFalse();
 
         [Theory]
-        [InlineData(0, 0, 2, 1, 0)]
-        [InlineData(0, 0, 1, 3, 1)]
-        [InlineData(2, 1, 1, 2, 1)]
-        public void WhenCheckingAnInvalidSlice_ShouldReturnFalse(int topX, int topY, int width, int height, int tolerance) =>
-            CheckSlice(topX, topY, width, height, tolerance).Should().BeFalse();
+        [InlineData(0, 0, 2, 1)]
+        [InlineData(0, 0, 1, 3)]
+        [InlineData(2, 1, 1, 2)]
+        public void WhenCheckingAnInvalidSlice_ShouldReturnFalse(int topX, int topY, int width, int height) =>
+            CheckSlice(topX, topY, width, height).Should().BeFalse();
 
         [Theory]
-        [InlineData(1, 0, 1, 2, 0)]
-        [InlineData(1, 0, 1, 3, 1)]
-        [InlineData(0, 0, 2, 3, 4)]
-        public void WhenCheckingAValidSlice_ShouldReturnTrue(int topX, int topY, int width, int height, int tolerance) =>
-            CheckSlice(topX, topY, width, height, tolerance).Should().BeTrue();
+        [InlineData(1, 0, 1, 2)]
+        [InlineData(1, 0, 1, 3)]
+        [InlineData(0, 0, 2, 3)]
+        public void WhenCheckingAValidSlice_ShouldReturnTrue(int topX, int topY, int width, int height) =>
+            CheckSlice(topX, topY, width, height).Should().BeTrue();
     }
 }
