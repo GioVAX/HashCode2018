4cd68e01de9dabe751a539e10f637a6297066a37
Giovanni Pezzino <giovannipezzino.64@gmail.com>
2019-02-07 16:32:12 +0000

    Added SlicesGenerator

---
+++ PracticeProblem/PracticeApp/SlicesGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace PracticeApp
+{
+    public class SlicesGenerator
+    {
+        public IEnumerable<Slice> GenerateAllSlices(int pizzaWidth, int pizzaHeight, IEnumerable<Size> sizes)
+        {
+            for (var row = 0; row < pizzaHeight; ++row)
+                for( var col = 0; col < pizzaWidth; ++col)
+                    foreach (var size in sizes)
+                        yield return new Slice(new Point(col, row), size);
+        }
+    }
+}
---
+++ PracticeProblem/PracticeAppUnitTests/SlicesGeneratorUnitTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using FluentAssertions;
+using PracticeApp;
+using Xunit;
+
+namespace PracticeAppUnitTests
+{
+    public class SlicesGeneratorUnitTests
+    {
+        private readonly SlicesGenerator _sut;
+        private readonly SliceSizes _sizes;
+
+        public SlicesGeneratorUnitTests()
+        {
+            _sut = new SlicesGenerator();
+            _sizes = new SliceSizes();
+        }
+
+        [Fact]
+        public void ShouldGenerateAllPossibleSlices()
+        {
+            var actual = _sut.GenerateAllSlices(2, 2, _sizes.Generate(2, 3));
+
+            actual.Should()
+                .NotBeNull()
+                .And.AllBeAssignableTo<Slice>()
+                .And.BeEquivalentTo(
+                    new List<Slice> {
+                        new Slice(new Point(0, 0), new Size( 1,2) ),
+                        new Slice(new Point(0, 0), new Size( 1,3) ),
+                        new Slice(new Point(0, 0), new Size( 2,1) ),
+                        new Slice(new Point(0, 0), new Size( 3,1) ),
+
+                        new Slice(new Point(0, 1), new Size( 1,2) ),
+                        new Slice(new Point(0, 1), new Size( 1,3) ),
+                        new Slice(new Point(0, 1), new Size( 2,1) ),
+                        new Slice(new Point(0, 1), new Size( 3,1) ),
+
+                        new Slice(new Point(1, 0), new Size( 1,2) ),
+                        new Slice(new Point(1, 0), new Size( 1,3) ),
+                        new Slice(new Point(1, 0), new Size( 2,1) ),
+                        new Slice(new Point(1, 0), new Size( 3,1) ),
+
+                        new Slice(new Point(1, 1), new Size( 1,2) ),
+                        new Slice(new Point(1, 1), new Size( 1,3) ),
+                        new Slice(new Point(1, 1), new Size( 2,1) ),
+                        new Slice(new Point(1, 1), new Size( 3,1) )
+                    }
+                );
+        }
+    }
+}
