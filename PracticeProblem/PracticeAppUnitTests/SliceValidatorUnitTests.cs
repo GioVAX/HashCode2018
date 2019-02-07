e7008cacd6250e987c851623a6bb06d2f544275f
Giovanni Pezzino <giovannipezzino.64@gmail.com>
2019-02-07 14:10:34 +0000

    Refactored unit tests

--- PracticeProblem/PracticeAppUnitTests/SliceValidatorUnitTests.cs
+++ PracticeProblem/PracticeAppUnitTests/SliceValidatorUnitTests.cs
@@ -20,44 +20,32 @@ namespace PracticeAppUnitTests
 
             _sut = new SliceValidator(ingredients);
         }
+        private bool CheckSlice(int topX, int topY, int width, int height, int tolerance)
+        {
+            var slice = new Slice(new Point(topX, topY), new SliceTemplate(width, height));
+            return _sut.IsSliceValid(slice, tolerance);
+        }
 
         [Theory]
         [InlineData(-1, 0, 2, 1)]
         [InlineData(0, -1, 2, 1)]
-        public void ValidatingSliceOutsideOfPizza_ShouldReturnFalse(int topX, int topY, int width, int height)
-        {
-            var slice = new Slice(new Point(topX, topY), new SliceTemplate(width, height));
+        public void ValidatingSliceOutsideOfPizza_ShouldReturnFalse(int topX, int topY, int width, int height) =>
+            CheckSlice(topX, topY, width, height, 0).Should().BeFalse();
 
-            _sut.IsSliceValid(slice, 0)
-                .Should().BeFalse();
-        }
+
 
         [Theory]
         [InlineData(0, 0, 2, 1, 0)]
         [InlineData(0, 0, 1, 3, 1)]
         [InlineData(2, 1, 1, 2, 1)]
-        public void WhenCheckingAnInvalidSlice_ShouldReturnFalse(int topX, int topY, int width, int height, int tolerance)
-        {
-            var slice = new Slice(new Point(topX, topY), new SliceTemplate(width, height));
-
-            _sut.IsSliceValid(slice, tolerance)
-                .Should().BeFalse();
-        }
+        public void WhenCheckingAnInvalidSlice_ShouldReturnFalse(int topX, int topY, int width, int height, int tolerance) =>
+            CheckSlice(topX, topY, width, height, tolerance).Should().BeFalse();
 
         [Theory]
         [InlineData(1, 0, 1, 2, 0)]
         [InlineData(1, 0, 1, 3, 1)]
         [InlineData(0, 0, 2, 3, 4)]
-        public void WhenCheckingAValidSlice_ShouldReturnTrue(int topX, int topY, int width, int height, int tolerance)
-        {
-            var slice = new Slice(new Point(topX, topY), new SliceTemplate(width, height));
-
-            _sut.IsSliceValid(slice, tolerance)
-                .Should().BeTrue();
-
-            _sut.IsSliceValid(new Slice(new Point(0, 0), new SliceTemplate(2, 3)), 4)
-                .Should().BeTrue();
-
-        }
+        public void WhenCheckingAValidSlice_ShouldReturnTrue(int topX, int topY, int width, int height, int tolerance) =>
+            CheckSlice(topX, topY, width, height, tolerance).Should().BeTrue();
     }
 }
