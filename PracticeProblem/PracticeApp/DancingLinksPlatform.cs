a40dfcc5b3b9e0e3b61750f41d4ff6a13f78afb2
Giovanni Pezzino <giovannipezzino.64@gmail.com>
2019-02-14 22:54:03 +0000

    DancingLinksPlatform

---
+++ PracticeProblem/PracticeApp/DancingLinksPlatform.cs
@@ -0,0 +1,7 @@
+﻿namespace PracticeApp
+{
+    public class DancingLinksPlatform
+    {
+
+    }
+}
---
+++ PracticeProblem/PracticeAppUnitTests/DancingLinksPlatformUnitTests.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using PracticeApp;
+
+namespace PracticeAppUnitTests
+{
+    public class DancingLinksPlatformUnitTests
+    {
+        private readonly DancingLinksPlatform _sut;
+
+        //[Fact]
+        public void CreatePlatform()
+        {
+            _sut.Should()
+                .NotBeNull();
+        }
+    }
+}
