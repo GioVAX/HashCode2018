04bba811ee3df286885b73f0ce9da8c297bf6879
Giovanni Pezzino <giovannipezzino.64@gmail.com>
2019-02-14 16:39:38 +0000

    DynProg stubs

---
+++ PracticeProblem/PracticeApp/DPPizzaSlicer.cs
@@ -0,0 +1,13 @@
+﻿namespace PracticeApp
+{
+    // ReSharper disable once InconsistentNaming
+    public class DPPizzaSlicer
+    {
+        private readonly PizzaDescription _pizza;
+
+        public DPPizzaSlicer(PizzaDescription pizza)
+        {
+            _pizza = pizza;
+        }
+    }
+}
---
+++ PracticeProblem/PracticeAppUnitTests/DPSlicerUnitTests.cs
@@ -0,0 +1,23 @@
+﻿using PracticeApp;
+using Xunit;
+
+namespace PracticeAppUnitTests
+{
+    // ReSharper disable once InconsistentNaming
+    public class DPSlicerUnitTests
+    {
+        private readonly DPPizzaSlicer _sut;
+
+        public DPSlicerUnitTests()
+        {
+            var pizza = new PizzaDescription(PizzaCases.Tiny);
+            _sut = new DPPizzaSlicer(pizza);
+        }
+
+        //[Fact]
+        public void WhenInitialized_ShouldContainWhereSlicesBeginAndEnd()
+        {
+
+        }
+    }
+}
